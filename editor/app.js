a45615090ef050e322b25d298430182530a626a3
anywhere3D <anywhere3d@gmail.com>
2018-06-12 03:29:28 +0200

    Update app.js

--- editor/app.js
+++ editor/app.js
@@ -1,6 +1,12 @@
 
 //  app.js
 
+    var debugMode;
+    var mouse = new THREE.Vector2();
+
+    var rendererHalfWidth = renderer.domElement.width * 0.5;
+    var rendererHalfHeight = renderer.domElement.height * 0.5;
+
     localPlayer.holder.visible = false;
     localPlayer.controller.center.set(0,1,0);
     localPlayer.controller.direction = Math.PI;
@@ -8,9 +14,13 @@
     animate();
 
     function animate(){
+
         windowAnimationFrameRequestID = requestAnimationFrame( animate );
+
         render();
+
         update();
+
     }
 
 //  Event Listeners.
@@ -21,4 +31,14 @@
         camera.aspect = container.clientWidth / container.clientHeight;
         camera.updateProjectionMatrix();
         renderer.setSize( container.clientWidth, container.clientHeight );
+        rendererHalfWidth = renderer.domElement.width * 0.5;
+        rendererHalfHeight = renderer.domElement.height * 0.5;
     }
+
+    $(window).on( "mousemove", onMouseMove );
+
+    function onMouseMove( event ) {
+        mouse.x = ( event.clientX - rendererHalfWidth );
+        mouse.y = ( event.clientY - rendererHalfHeight );
+    }
+
